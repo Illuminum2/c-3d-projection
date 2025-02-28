e3e7b194144e0bdbacd5779dc4796d99a1543f35
Illuminum2 <trost.zureden08@icloud.com>
2025-02-28 23:53:01 +0100

    Refactor: Update run link in main.c metadata

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 //
 //  Created by Illuminum on 2/24/25 for the Blackbox YSWS
 //  Repo: https://github.com/Illuminum2/c-3d-projection
-//  Run: https://blackbox.hackclub.com/editor/?code=2e5f68
+//  Run: https://blackbox.hackclub.com/editor/?code=96c0ac
 //  Semi-port of my Pygame 3D projection project to C, heavily modified
 //
 
